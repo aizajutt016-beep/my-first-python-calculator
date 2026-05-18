285670bfbb3e02741a1d64bfee831cc84b3fb0e2
Aiza Zafar <aizajutt016@gmail.com>
2026-05-18 03:03:55 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+
+import math 
+
+def S_add(a,b):
+    print("my add result is this")
+    return print(a+b)
+
+def S_sub(a,b):
+    print("my sub result is this")
+    return print(a-b)
+
+def S_divide(a, b):
+    print ("my divide result is this")
+    return print(a/b)
+
+def S_multiply(a,b):
+    print("my mukltply result is this")
+    return print(a*b)
+
+def S_mod(a,b):
+    print ("my mod result is this")
+    return print (a%b)
+
+def S_square(a):
+    print ("my square result is this")
+    return print (a**2)
+
+def S_log(a, b):
+    print ("my log result is this")
+    return print (math.log(a, b)) #math.log (a, b)
+
+
+S_add(90,50)
+S_add(87,26)
+
+S_sub(256,90)
+S_sub(56,190)
+
+S_multiply(4, 2)
+S_multiply(24,6)
+
+S_divide(100, 5)
+S_divide(36, 5)
+
+S_mod(23,5)
+S_mod(5,4)
+
+S_square(28)
+S_square(7)
+
+S_log(10,8)
+S_log(9,100)
+
+
+
+  
